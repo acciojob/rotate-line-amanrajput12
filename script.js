fb57822ed5a40719f3187fc53ec5fdd74210d976
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-03-24 13:32:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+window.addEventListener("load", () => {
+    let angle = 0;
+    const line = document.getElementById("line");
+	line.style.width="200px";
+	line.style.height="2px";
+	line.style.backgroundColor="#000000"
+	line.style.position="absolute"
+	line.style.top="300px";
+	line.style.left="300px"
+
+    setInterval(() => {
+        angle += 2; 
+        line.style.transform = `rotate(${angle}deg)`;
+    }, 20);
+});
